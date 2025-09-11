bc2dacae025c9d4dd20a761bd55d81967ed9e27a
danishyarkhwork <danishyarkhwork@gmail.com>
2025-09-11 10:21:52 +0430

    Remove post seeding and image backfilling from DatabaseSeeder for cleaner initialization

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use App\Models\Post;
 use Illuminate\Support\Facades\Hash;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -25,25 +24,5 @@ class DatabaseSeeder extends Seeder
                 'email_verified_at' => now(),
             ]
         );
-
-        // Seed dummy posts
-        Post::factory()->count(18)->create();
-
-        // Backfill images for posts without image_path
-        $sampleImages = [
-            'assets/images/1.jpg',
-            'assets/images/3.jpg',
-            'assets/images/4.jpg',
-            'assets/images/5.jpg',
-            'assets/images/banners/banner-1.jpeg',
-            'assets/images/banners/banner-2.jpeg',
-            'assets/images/banners/banner-3.jpeg',
-            'assets/images/banners/banner-4.jpeg',
-            'assets/images/banners/banner-5.jpeg',
-            'assets/images/banners/banner-6.jpeg',
-        ];
-        Post::whereNull('image_path')->orWhere('image_path', '=', '')->get()->each(function ($post) use ($sampleImages) {
-            $post->update(['image_path' => $sampleImages[array_rand($sampleImages)]]);
-        });
     }
 }
